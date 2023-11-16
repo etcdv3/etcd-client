2ed09f6bd7dca0f5048c0849b81acead8ef4ffb6
tison <wander4096@gmail.com>
2023-11-16 14:56:23 +0800

    Update src/rpc/kv.rs

--- src/rpc/kv.rs
+++ src/rpc/kv.rs
@@ -569,7 +569,7 @@ impl DeleteResponse {
         unsafe { &*(self.0.prev_kvs.as_slice() as *const _ as *const [KeyValue]) }
     }
 
-    /// If `prev_kv` is set in the request, take the previous key-value pairs, leaving an empty vector in its place.
+    /// If `prev_kvs` is set in the request, take the previous key-value pairs, leaving an empty vector in its place.
     #[inline]
     pub fn take_prev_kvs(&mut self) -> Vec<KeyValue> {
         self.0.prev_kvs.drain(..).map(KeyValue::new).collect()
