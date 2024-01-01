1519b92e97b36a55c740a570486db9e2c69f8fe9
David Li <davidli2010@foxmail.com>
2024-01-01 11:15:52 +0800

    Add namespace example

--- tests/namespace.rs
+++ tests/namespace.rs
@@ -1,22 +1,41 @@
 mod testing;
 
 use crate::testing::{get_client, Result};
-use etcd_client::KvClientPrefix;
+use etcd_client::{DeleteOptions, KvClientPrefix};
 
 #[tokio::test]
-async fn test_namespace_put_get() -> Result<()> {
+async fn test_namespace_kv() -> Result<()> {
     let mut client = get_client().await?;
     let mut c = KvClientPrefix::new(client.kv_client(), "foo/".into());
 
     c.put("abc", "bar", None).await?;
-    let kvs = c.get("abc", None).await?.take_kvs();
-    assert_eq!(kvs.len(), 1);
-    assert_eq!(kvs[0].key(), "abc".as_bytes());
-    assert_eq!(kvs[0].value(), "bar".as_bytes());
 
-    let kvs = client.get("foo/abc", None).await?.take_kvs();
-    assert_eq!(kvs.len(), 1);
-    assert_eq!(kvs[0].key(), "foo/abc".as_bytes());
-    assert_eq!(kvs[0].value(), "bar".as_bytes());
+    // get kv
+    {
+        let kvs = c.get("abc", None).await?.take_kvs();
+        assert_eq!(kvs.len(), 1);
+        assert_eq!(kvs[0].key(), b"abc");
+        assert_eq!(kvs[0].value(), b"bar");
+    }
+
+    // get prefixed kv
+    {
+        let kvs = client.get("foo/abc", None).await?.take_kvs();
+        assert_eq!(kvs.len(), 1);
+        assert_eq!(kvs[0].key(), b"foo/abc");
+        assert_eq!(kvs[0].value(), b"bar");
+    }
+
+    // delete kv
+    {
+        let resp = c
+            .delete("abc", Some(DeleteOptions::new().with_prev_key()))
+            .await?;
+        assert_eq!(resp.deleted(), 1);
+        let kvs = resp.prev_kvs();
+        assert_eq!(kvs[0].key(), b"abc");
+        assert_eq!(kvs[0].value(), b"bar");
+    }
+
     Ok(())
 }
